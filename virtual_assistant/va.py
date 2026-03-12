b52737ff6d578aafa1d8c8de74e52f282c61acf6
Austin <austin.ramirez1@outlook.com>
2026-03-12 13:47:01 -0500

    importing necessary modules

--- virtual_assistant/va.py
+++ virtual_assistant/va.py
@@ -1,5 +1,9 @@
 import os
 from dotenv import load_dotenv
+from elevenlabs.client import ElevenLabs
+from elevenlabs.conversational_ai.conversation import Conversation
+from elevenlabs.conversational_ai.default_audio_interface import DefaultAudioInterface
+from elevenlabs.types import ConversationConfig
 
 load_dotenv()
 
