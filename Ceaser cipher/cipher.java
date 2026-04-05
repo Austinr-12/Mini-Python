9573dde51d7a3d09140f7d6e5879b01c7cfade1b
Austin <austin.ramirez1@outlook.com>
2026-04-05 15:00:41 -0500

    fixing where I put stringbuilder

--- Ceaser cipher/cipher.java
+++ Ceaser cipher/cipher.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 
 // method that will do the encrypting
 public String encrypt(String text, int shift) {
-    
+    StringBuilder result = new StringBuilder();
 }
 
-StringBuilder result = new StringBuilder();
