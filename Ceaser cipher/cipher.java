278776ba47ee132abc2a6df8861937774b4be94c
Austin <austin.ramirez1@outlook.com>
2026-04-08 17:39:05 -0500

    for loop within encrypting function

--- Ceaser cipher/cipher.java
+++ Ceaser cipher/cipher.java
@@ -4,5 +4,11 @@ import java.util.Scanner;
 // method that will do the encrypting
 public String encrypt(String text, int shift) {
     StringBuilder result = new StringBuilder();
+
+    // for-each loop
+    for (char character : text.toCharArray()) {
+        // do something with character
+    }
+
 }
 
