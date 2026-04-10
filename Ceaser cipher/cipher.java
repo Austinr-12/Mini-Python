31a8dbee6ae307899b5d3485b82eb5fbbb14003e
Austin <austin.ramirez1@outlook.com>
2026-04-10 16:20:19 -0500

    reformatting and adding return to encrypt function

--- Ceaser cipher/cipher.java
+++ Ceaser cipher/cipher.java
@@ -2,18 +2,19 @@ import java.util.Scanner;
 
 
 // method that will do the encrypting
-public String encrypt(String text, int shift) {
+public static String encrypt(String text, int shift) {
     StringBuilder result = new StringBuilder();
 
-    // for-each loop
     for (char character : text.toCharArray()) {
         if (Character.isLetter(character)) {
-        char shifted = (char) ((character - base + shift) % 26 + base);
+            char base = Character.isLowerCase(character) ? 'a' : 'A';
+            char shifted = (char) ((character - base + shift) % 26 + base);
             result.append(shifted);
         } else {
             result.append(character);
         }
     }
 
+    return result.toString();
 }
 
